ccac9c4f7ac522883d138902cc4aed1fc60b0faa
Andreas Steinmair (7AAIF) <andreas.steinmair@edu.htl-klu.at>
2023-10-11 19:27:28 +0200

    dfhd

--- src/main/java/at/htlklu/spring/controller/DepartmentController.java
+++ src/main/java/at/htlklu/spring/controller/DepartmentController.java
@@ -14,33 +14,38 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
+//localhost:8082/mvc/teachers
+//localhost:8082/mvc/teachers/1/departments
+//localhost:8082/mvc/teachers/1/schoolClasses
+
+
 @Controller
-@RequestMapping(value ="mvc/teachers2")
-public class Teacher2Controller
+@RequestMapping(value ="mvc/departments")
+public class DepartmentController
 {
 	//region Properties
-	private static Logger logger = LogManager.getLogger(Teacher2Controller.class);
-	private static final String CLASS_NAME = "TeacherController";
+	private static Logger logger = LogManager.getLogger(DepartmentController.class);
+	private static final String CLASS_NAME = "DepartmentController";
 	public static final String FORM_NAME_SINGLE = "TeacherSingle";
-	public static final String FORM_NAME_LIST = "TeacherList";
+	public static final String FORM_NAME_LIST = "DepartmentList";
 
 	@Autowired
-	TeacherRepository teacherRepository;
+	DepartmentRepository departmentRepository;
 	//endregion
 
 
-	// localhost:8082/mvc/teachers2/show
-	@GetMapping("show")
+	// localhost:8082/mvc/teachers
+	@GetMapping("")
 	public ModelAndView show()
 	{
 		logger.info(LogUtils.info(CLASS_NAME, "show"));
 
 		ModelAndView mv = new ModelAndView();
 
-		mv.setViewName(FORM_NAME_LIST);
+		mv.setViewName(DepartmentController.FORM_NAME_LIST);
 
-		List<Teacher> teachers = teacherRepository.findAll();
-		mv.addObject("teachers", teachers);
+		List<Department> departments = departmentRepository.findAll();
+		mv.addObject("departments", departments); //link zur HTML
 
 	    return mv;
 	}
--- src/main/java/at/htlklu/spring/controller/StartController.java
+++ src/main/java/at/htlklu/spring/controller/StartController.java
@@ -68,4 +68,5 @@ public class StartController
 		return teacher;
 	}
 
+
 }
