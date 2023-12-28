2cec91692974364bd78999581843b4b4d5b38002
Andre <andreas.steinmair@edu.htl-klu.at>
2023-12-28 16:10:15 +0100

    added swagger

--- src/main/java/at/htlklu/bavi/Application.java
+++ src/main/java/at/htlklu/bavi/Application.java
@@ -4,18 +4,27 @@ package at.htlklu.bavi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableJpaAuditing
-//@EnableSwagger2
+@EnableSwagger2
 public class Application
 {
 	public static void main(String[] args)
 	{		
 		SpringApplication.run(Application.class, args);
 
+	}
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("at.htlklu.bavi.controller")).build();
 	}
 }
